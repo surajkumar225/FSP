ce72fcb91bb240446eab7357a84ad612e61d405e
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2023-07-25 12:27:50 +0530

    FSP

---
+++ script.js
@@ -0,0 +1,121 @@
+console.log("Hey Trinity------------------------");
+let firstname = "Surk";
+let lastname = "Dawson";
+let videos = 10;
+let subscribers = 6000;
+let product = ["Hero", "Alom", "Jack", "And", "Oggy"];
+// let update = ["pizza", "water"];
+
+// console.log(firstname);
+// console.log(firstname + " " + lastname);
+
+// console.log(firstname.startsWith("S"));
+// console.log(firstname.endsWith("k"));
+// console.log(firstname.includes("Sur"));
+
+// let htmlTemplate = `<h2>${firstname}${lastname}</h2>
+// <p>videos: ${videos}</p>
+// <p>Subcribers: ${subscribers}</p>`;
+// console.log(htmlTemplate);
+
+// let productMethod = product.join(",");
+// console.log(productMethod);
+// console.log(product);
+
+// productMethod = product.concat(update);
+// console.log(productMethod);
+// console.log(product);
+
+// productMethod = product.push("water");
+// console.log(productMethod);
+// console.log(product);
+
+// let score = 25;
+// console.log(score);
+// let update = null;
+// console.log(update);
+// console.log(update, update + 30, `My score is ${score}`);
+
+// console.log(10 == "10");
+// console.log(10 == "99");
+// console.log(10 != "99");
+// console.log(10 != "10");
+// let a = 10;
+// let b = "10";
+// let c = 10;
+// console.log(a === b);
+// console.log(a === c);
+
+// const students = ["Oggy", "Jack", "Micheal", "Daw"];
+// console.log("Trinity");
+// for (let student of students) {
+//   console.log(student);
+// }
+// for (let index in students) {
+//   console.log(students[index]);
+// }
+// const student = { username: "Surk", age: 22 };
+// for (let key in student) {
+//   console.log(students[key]);
+// }
+// const username = "Eddy";
+// for (let character of username) {
+//   console.log(character);
+// }
+
+// function addition(numOne, numTwo) {
+//   return numOne + numTwo;
+// }
+// console.log(addition(2, 5));
+// console.log(addition(5, 10));
+// console.log(addition(100, 500));
+// console.log(addition(2, 0.5));
+// console.log(addition(900, 0.1));
+
+// let intro = function () {
+//   console.log("HeyIntro");
+// };
+// intro();
+
+// function fullname(firstname = "Surk", lastname = "Dawson") {
+//   console.log(`Welcome ${firstname} ${lastname}`);
+// }
+// fullname("Trinity", "Daw");
+// fullname();
+
+// let arrow = (name) => {
+//   console.log("Hey", name);
+// };
+// arrow("Surk");
+
+// let character = ["Hero", "Alom", "Jack", "And", "Oggy"];
+// character.forEach((character) => console.log(character));
+// let students = ["Surk", "Dawson"];
+// students.forEach((students, index) => {
+//   console.log(students, index);
+// });
+
+// const movies = [
+//   { name: "Coffee Day", rating: 9 },
+//   { name: "The lost coder", rating: 7 },
+// ];
+// const fiterdmovies = movies.filter((movie) => {
+//   return movie.rating > 6;
+// });
+// console.log(fiterdmovies);
+
+// let num = [2, 4, 5, 6, 7, 8];
+// const getEven = num.filter((numbers) => {
+//   if (numbers % 2 == 0) {
+//     return numbers;
+//   }
+// });
+// console.log(getEven);
+
+// const num = [20, 40, 60];
+// const mapNumbers = num.map((num) => {
+//   return num + 5;
+// });
+// console.log(mapNumbers);
+
+// -------------------------------------------------------------------------------------------------------------------
